3d3981b19613b4cef7de446a8cb7e270b7a0c4ee
alienwizard <johanwendin@gmail.com>
2016-11-11 13:54:36 +0100

    fixed pagination

--- wp-content/themes/stockholm/template-parts/Juridik.php
+++ wp-content/themes/stockholm/template-parts/Juridik.php
@@ -15,89 +15,107 @@ get_header(2); ?>
 
 <div class="inner-page">
 	<div class="container page-top">
+
   		<div class="vara-btm scrl-1 pdng0">
+				<div class="law-image" style="background-image:url('<?php the_post_thumbnail_url('full'); ?>')"></div>
+				<div class="law-content">
   			<?php
       			global $post; $post = get_post( 193 , OBJECT ); setup_postdata( $post );
       		?>
-      		<h3><?php echo get_the_title(193); ?></h3>   	
+      		<h3><?php echo get_the_title(193); ?></h3>
 	      <?php
-	      	echo apply_filters( 'the_content', $post->post_content ); 
+	      	echo apply_filters( 'the_content', $post->post_content );
 	      	wp_reset_postdata();
 	      ?>
+			</div>
+			<div class="clearfix"></div>
       	</div>
    		<div class="vara-btm scrl-2 pdng0">
+				<div class="law-image" style="background-image:url('<?php the_post_thumbnail_url('full'); ?>')"></div>
+				<div class="law-content">
 	  	  	<hr>
 			<?php
-				global $post; $post = get_post( 195 , OBJECT ); setup_postdata( $post ); 
+				global $post; $post = get_post( 195 , OBJECT ); setup_postdata( $post );
 			?>
-      	<h3><?php echo get_the_title(195); ?></h3>   	
+      	<h3><?php echo get_the_title(195); ?></h3>
       	<?php
-      		echo apply_filters( 'the_content', $post->post_content ); 
+      		echo apply_filters( 'the_content', $post->post_content );
       		wp_reset_postdata();
       	?>
+			</div>
+			<div class="clearfix"></div>
 		</div>
        	<div class="vara-btm scrl-3 pdng0">
+					<div class="law-image" style="background-image:url('<?php the_post_thumbnail_url('full'); ?>')"></div>
+					<div class="law-content">
   	  		<hr>
       		<?php
-      			global $post; $post = get_post( 197 , OBJECT ); setup_postdata( $post ); 
+      			global $post; $post = get_post( 197 , OBJECT ); setup_postdata( $post );
       		?>
-      		<h3><?php echo get_the_title(197); ?></h3>   	
+      		<h3><?php echo get_the_title(197); ?></h3>
       		<?php
-      			echo apply_filters( 'the_content', $post->post_content ); 
+      			echo apply_filters( 'the_content', $post->post_content );
       			wp_reset_postdata();
       		?>
       	</div>
+				<div class="clearfix"></div>
+			</div>
   	 	<div class="vara-btm scrl-4">
+				<div class="law-image" style="background-image:url('<?php the_post_thumbnail_url('full'); ?>')"></div>
+				<div class="law-content">
   	  		<hr>
       		<?php
-      			global $post; $post = get_post( 199 , OBJECT ); setup_postdata( $post ); 
+      			global $post; $post = get_post( 199 , OBJECT ); setup_postdata( $post );
       		?>
-      		<h3><?php echo get_the_title(199); ?></h3>   	
+      		<h3><?php echo get_the_title(199); ?></h3>
       		<?php
-      			echo apply_filters( 'the_content', $post->post_content );  
+      			echo apply_filters( 'the_content', $post->post_content );
       			wp_reset_postdata();
   			?>
+			</div>
+						<div class="clearfix"></div>
       </div>
+
     </div>
 </div>
 
 <script>
 	jQuery(document).ready(function($){
 		$('.bouppteckning > a').click(function(){
-			
+
 			$('html, body').animate({
 	        scrollTop: $('.scrl-1').offset().top -190
 	    }, 500);
-	    
+
 		});
-		 
+
 		 		$('.arvskifte > a').click(function(){
-			
+
 			$('html, body').animate({
 	         scrollTop: $('.scrl-2').offset().top -170
 	    }, 500);
-	    
+
 		});
-		 
+
 		 $('.fastighetsförsäljning > a').click(function(){
-			
+
 			$('html, body').animate({
-	          scrollTop: $('.scrl-3').offset().top -170 
+	          scrollTop: $('.scrl-3').offset().top -170
 	    }, 500);
-	    
+
 		});
-		
-		
+
+
 		 $('.testamente > a').click(function(){
-			
+
 			$('html, body').animate({
 	          scrollTop: $('.scrl-4').offset().top -120
 	    }, 500);
-	    
+
 		});
-		
+
 	});
-	
+
 </script>
 
 
